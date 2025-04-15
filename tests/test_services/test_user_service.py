473ebb25678b7e48c18f8e565bcd4e14e3acad11
Seniz <so9@njit.edu>
2025-04-14 21:09:53 -0400

    Fix: mail system functionality

--- tests/test_services/test_user_service.py
+++ tests/test_services/test_user_service.py
@@ -8,7 +8,6 @@ from app.services.user_service import UserService
 pytestmark = pytest.mark.asyncio
 
 # Test creating a user with valid data
-@pytest.mark.skip(reason="Email service not part of assignment")
 async def test_create_user_with_valid_data(db_session, email_service):
     user_data = {
         "email": "valid_user@example.com",
@@ -19,7 +18,6 @@ async def test_create_user_with_valid_data(db_session, email_service):
     assert user.email == user_data["email"]
 
 # Test creating a user with invalid data
-@pytest.mark.skip(reason="Email service not working, don't know how to fix")
 async def test_create_user_with_invalid_data(db_session, email_service):
     user_data = {
         "nickname": "",  # Invalid nickname
@@ -92,7 +90,6 @@ async def test_list_users_with_pagination(db_session, users_with_same_role_50_us
     assert users_page_1[0].id != users_page_2[0].id
 
 # Test registering a user with valid data
-@pytest.mark.skip(reason="Email service not working, don't know how to fix")
 async def test_register_user_with_valid_data(db_session, email_service):
     user_data = {
         "email": "register_valid_user@example.com",
